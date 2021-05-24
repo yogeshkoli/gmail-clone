c81af34b8c0bcd7b3d4c32082dddc02c96d312fc
ykoli <ykoli@ascllc.net>
2021-05-24 17:06:17 +0530

    Added new footer icons for sidebar

--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Sidebar.scss';
 import AddIcon from '@material-ui/icons/Add';
 import SidebarOption from './SidebarOption';
-import { Button } from '@material-ui/core';
+import { Button, IconButton } from '@material-ui/core';
 import InboxIcon from '@material-ui/icons/Inbox';
 import StarIcon from '@material-ui/icons/Star';
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
@@ -10,6 +10,9 @@ import LabelImportantIcon from '@material-ui/icons/LabelImportant';
 import NearMeIcon from '@material-ui/icons/NearMe';
 import NoteIcon from '@material-ui/icons/Note';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import PersonIcon from '@material-ui/icons/Person';
+import DuoIcon from '@material-ui/icons/Duo';
+import PhoneIcon from '@material-ui/icons/Phone';
 
 function Sidebar() {
     return (
@@ -28,6 +31,20 @@ function Sidebar() {
             <SidebarOption Icon={NearMeIcon} title="Sent" number={40} />
             <SidebarOption Icon={NoteIcon} title="Drafts" number={40} />
             <SidebarOption Icon={ExpandMoreIcon} title="More" number={40} />
+
+            <div className="sidebar__footer">
+                <div className="sidebar__footer__icons">
+                    <IconButton>
+                        <PersonIcon />
+                    </IconButton>
+                    <IconButton>
+                        <DuoIcon />
+                    </IconButton>
+                    <IconButton>
+                        <PhoneIcon />
+                    </IconButton>
+                </div>
+            </div>
         </div>
     );
 }
